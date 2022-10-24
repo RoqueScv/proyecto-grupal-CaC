1c6ad6eff9b0ae51595756508ce42f1e957c6442
RoqueScv <roque.scovenna@gmail.com>
2022-10-24 10:59:09 -0300

    register

--- js/formulario.js
+++ js/formulario.js
@@ -1,51 +1,49 @@
 function validacion() 
-                        {
-                            valorNombre = document.getElementById("nombre").value;
-                            valorTelefono = document.getElementById("telefono").value;
-                            valorEmail = document.getElementById("email").value;
-                            valorAsunto = document.getElementById("asunto").value;
-                            valorMensaje = document.getElementById("message").value;
-
-                            if(valorNombre == null || valorNombre.length < 3) {
-                                document.getElementById('mensaje-nombre').innerHTML = "Tenes que completar el Nombre!";
-                                elemento = document.getElementById('nombre');
-                                elemento.focus();
-                                return false;
-                            }   
-                            document.getElementById('mensaje-nombre').innerHTML = "";
-                            if(isNaN(valorTelefono) || valorTelefono.length > 12 || valorTelefono.length < 8 ) {
-                                document.getElementById('mensaje-telefono').innerHTML = "El telefono debe ser un numero de 8-12 cifras!";
-                                elemento = document.getElementById('telefono');
-                                elemento.focus();
-                                
-                                return false;
-                            }
-                            document.getElementById('mensaje-telefono').innerHTML = "";
-                            if(!(/^\w+([\.-]?\w+)*@(?:|hotmail|outlook|yahoo|live|gmail)\.(?:|com|ar)+$/.test(valorEmail))) {
-                                document.getElementById('mensaje-email').innerHTML = "No es una direccion de email correcta" ;
-                                        
-                                elemento = document.getElementById('email');
-                                elemento.focus();
-                                    
-                                return false;
-                            }
-                            document.getElementById('mensaje-email').innerHTML = ""
-                            if(valorAsunto == null || valorAsunto.length < 3) {
-                                document.getElementById('mensaje-asunto').innerHTML = "Tenes que completar el Asunto!";
-                                elemento = document.getElementById('asunto');
-                                elemento.focus();
-
-                                return false;
-                            }
-                            document.getElementById('mensaje-asunto').innerHTML = "";
-                            if(valorMensaje == null || valorMensaje.length < 3) {
-                                document.getElementById('mensaje-mensaje').innerHTML = "Tenes que completar el Mensaje!";
-                                elemento = document.getElementById('message');
-                                elemento.focus();
-
-                                return false;
-                            }
-                            document.getElementById('mensaje-mensaje').innerHTML = "";
-                        return true;
-                        }
+{
+    valorNombre = document.getElementById("nombre").value;
+    valorTelefono = document.getElementById("telefono").value;
+    valorEmail = document.getElementById("email").value;
+    valorAsunto = document.getElementById("asunto").value;
+    valorMensaje = document.getElementById("message").value;
+    if(valorNombre == null || valorNombre.length < 3) {
+        document.getElementById('mensaje-nombre').innerHTML = "Tenes que completar el Nombre!";
+        elemento = document.getElementById('nombre');
+        elemento.focus();
+        return false;
+    }   
+    document.getElementById('mensaje-nombre').innerHTML = "";
+    if(isNaN(valorTelefono) || valorTelefono.length > 12 || valorTelefono.length < 8 ) {
+        document.getElementById('mensaje-telefono').innerHTML = "El telefono debe ser un numero de 8-12 cifras!";
+        elemento = document.getElementById('telefono');
+        elemento.focus();
+        
+        return false;
+    }
+    document.getElementById('mensaje-telefono').innerHTML = "";
+    if(!(/^\w+([\.-]?\w+)*@(?:|hotmail|outlook|yahoo|live|gmail)\.(?:|com|ar)+$/.test(valorEmail))) {
+        document.getElementById('mensaje-email').innerHTML = "No es una direccion de email correcta" ;
+                
+        elemento = document.getElementById('email');
+        elemento.focus();
+            
+        return false;
+    }
+    document.getElementById('mensaje-email').innerHTML = ""
+    if(valorAsunto == null || valorAsunto.length < 3) {
+        document.getElementById('mensaje-asunto').innerHTML = "Tenes que completar el Asunto!";
+        elemento = document.getElementById('asunto');
+        elemento.focus();
+        return false;
+    }
+    document.getElementById('mensaje-asunto').innerHTML = "";
+    if(valorMensaje == null || valorMensaje.length < 3) {
+        document.getElementById('mensaje-mensaje').innerHTML = "Tenes que completar el Mensaje!";
+        elemento = document.getElementById('message');
+        elemento.focus();
+        return false;
+    }
+    document.getElementById('mensaje-mensaje').innerHTML = "";
+    
+    return true;
+}
 
--- js/register.js
+++ js/register.js
@@ -1,65 +1,59 @@
-function validacion()
+function validacion2()
+{
+    valorNombre = document.getElementById("nombre2").value;
+    valorApellido = document.getElementById("apellido2").value;
+    valorEmail = document.getElementById("email2").value;
+    valorContraseña = document.getElementById("password2").value;
+    valorConfirma = document.getElementById("confirma-contraseña2").value;
+    valorTelefono = document.getElementById("telefono2").value;
 
-                        {
-                            valorNombre = document.getElementById("nombre").value;
-                            valorApellido = document.getElementById("apellido").value;
-                            valorEmail = document.getElementById("email").value;
-                            valorContraseña = document.getElementById("Contraseña").value;
-                            valorConfirma = document.getElementById("confirma contraseña").value;
-                            valorTelefono = document.getElementById("telefono").value;
 
-
-                            if(valorNombre == null || valorNombre.length < 3) {
-                                document.getElementById('mensaje-nombre').innerHTML = "Tenes que completar el Nombre!";
-                                elemento = document.getElementById('nombre');
-                                elemento.focus();
-                                return false;
-                            }   
-                            if(valorApellido == null || valorApellido.length < 3) {
-                                document.getElementById('mensaje-apellido').innerHTML = "Tenes que completar el Apellido!";
-                                elemento = document.getElementById('apellido');
-                                elemento.focus();
-                                return false;
-                            }   
-                          
-                            }
-                            document.getElementById('mensaje-email').innerHTML = "";
-                            if(!(/^\w+([\.-]?\w+)*@(?:|hotmail|outlook|yahoo|live|gmail)\.(?:|com|ar)+$/.test(valorEmail))) {
-                                document.getElementById('mensaje-email').innerHTML = "No es una direccion de email correcta" ;
-                                        
-                                elemento = document.getElementById('email');
-                                elemento.focus();
-                                    
-                                return false;
-                            }
-                            document.getElementById('mensaje-contraseña').innerHTML = ""
-                            if(valorAsunto == null || valorAsunto.length < 3) {
-                                document.getElementById('mensaje-contraseña').innerHTML = "Tenes que ingresar una contraseña!";
-                                elemento = document.getElementById('contraseña');
-                                elemento.focus();
-
-                                return false;
-                            }
-
-                            document.getElementById('mensaje-confirma').innerHTML = "";
-                            if(valorMensaje == null || valorMensaje.length < 3) {
-                                document.getElementById('mensaje-confirma').innerHTML = "Tenes que confirmar la contraseña!";
-                                elemento = document.getElementById('confirma');
-                                elemento.focus();
-
-                                return false;
-                            }
-
-                            document.getElementById('mensaje-telefono').innerHTML = "";
-                            if(isNaN(valorTelefono) || valorTelefono.length > 12 || valorTelefono.length < 8 ) {
-                                document.getElementById('mensaje-telefono').innerHTML = "El telefono debe ser un numero de 8-12 cifras!";
-                                elemento = document.getElementById('telefono');
-                                elemento.focus();
-                                
-                                return false;
-                            }
-
-                            document.getElementById('mensaje-mensaje').innerHTML = "";
-                                return true;
+    if(valorNombre == null || valorNombre.length < 3) {
+        document.getElementById('mensaje-nombre').innerHTML = "Tenes que completar el Nombre!";
+        elemento = document.getElementById('nombre2');
+        elemento.focus();
+        return false;
+    }   
+    if(valorApellido == null || valorApellido.length < 3) {
+        document.getElementById('mensaje-apellido').innerHTML = "Tenes que completar el Apellido!";
+        elemento = document.getElementById('apellido2');
+        elemento.focus();
+        return false;
+    }   
+  
+    }
+    document.getElementById('mensaje-email').innerHTML = "";
+    if(!(/^\w+([\.-]?\w+)*@(?:|hotmail|outlook|yahoo|live|gmail)\.(?:|com|ar)+$/.test(valorEmail))) {
+        document.getElementById('mensaje-email').innerHTML = "No es una direccion de email correcta" ;
+                
+        elemento = document.getElementById('email2');
+        elemento.focus();
+            
+        return false;
+    }
+    document.getElementById('mensaje-contraseña').innerHTML = ""
+    if(valorAsunto == null || valorAsunto.length < 3) {
+        document.getElementById('mensaje-contraseña').innerHTML = "Tenes que ingresar una contraseña!";
+        elemento = document.getElementById('password');
+        elemento.focus();
+        return false;
+    }
+    document.getElementById('mensaje-confirma').innerHTML = "";
+    if(valorMensaje == null || valorMensaje.length < 3) {
+        document.getElementById('mensaje-confirma').innerHTML = "Tenes que confirmar la contraseña!";
+        elemento = document.getElementById('confirm-password');
+        elemento.focus();
+        return false;
+    }
+    document.getElementById('mensaje-telefono').innerHTML = "";
+    if(isNaN(valorTelefono) || valorTelefono.length > 12 || valorTelefono.length < 8 ) {
+        document.getElementById('mensaje-telefono').innerHTML = "El telefono debe ser un numero de 8-12 cifras!";
+        elemento = document.getElementById('telefono');
+        elemento.focus();
+        
+        return false;
+    }
+    document.getElementById('mensaje-mensaje').innerHTML = "";
+        return true;
                         
 
